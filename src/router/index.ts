f5bbe90ac970b20a575c90cf154e8af916dea816
杨国涣 <46448013+yangguohuan@users.noreply.github.com>
2025-03-23 16:47:09 +0800

    功能：路由懒加载
    功能：路由懒加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      component: () => import('@/views/loginPage.vue'),
+    },
+    {
+      path: '/home',
+      component: () => import('@/views/homePage.vue'),
+    },
+    {
+      path: '/404',
+      component: () => import('@/views/404Page.vue'),
+    },
+  ],
 })
 
 export default router
