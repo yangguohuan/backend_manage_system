cb039c19e44dbc1ff5f4c512526f8df364c16ab1
杨国涣 <46448013+yangguohuan@users.noreply.github.com>
2025-03-23 09:42:36 +0800

    功能：封装了svg组件
    功能：封装了svg组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <svgIcon name="home"></svgIcon>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import svgIcon from './components/svg-icon.vue'
+</script>
 
 <style scoped></style>
---
+++ src/components/svg-icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg :style="{ width, height }">
+    <use :xlink:href="prefix + name" :fill="color"></use>
+  </svg>
+</template>
+<script lang="ts" setup>
+defineProps({
+  // 字符串前缀
+  prefix: {
+    type: String,
+    default: '#icon-',
+  },
+
+  // svg 图标名称
+  name: String,
+
+  // 需要的颜色
+  color: {
+    type: String,
+    default: '',
+  },
+
+  // 传入的大小，宽和高
+  width: {
+    type: String,
+    default: '16px',
+  },
+  height: {
+    type: String,
+    default: '16px',
+  },
+})
+</script>
+<style scoped></style>
